10646be353ef4added5b690025e9c3c988b9a802
hongzzi-zzi <hongzzi-zzi@gmail.com>
2022-06-28 13:17:43 +0900

    mask2label 변경

--- mapping.py
+++ mapping.py
@@ -10,14 +10,22 @@ PIL2tensor=transforms.ToTensor()
 mapping={(0, 0, 0):0,
          (255, 255, 255):1}#(0, 0, 0): background, (255, 255, 255): teeth
 #%%
-def mask2label(mask_ori):## mask_ori: RGB tensor
-    mask=tensor2PIL(mask_ori)# PIL
-    pix=mask.load()# Pixel
-    w, h=mask.size
-    label = torch.zeros(2, h, w, dtype=torch.float)# new tensir(CHW)
-    for k in mapping:
-        v=mapping.get(k)
-        # w:가로 h: 세로
-        for w, h in itertools.product(range(w),range(h)):
-            label[v][h][w]=(pix[w, h]==k)
+# def mask2label(mask_ori):## mask_ori: RGB tensor
+#     mask=tensor2PIL(mask_ori)# PIL
+#     pix=mask.load()# Pixel
+#     w, h=mask.size
+#     label = torch.zeros(2, h, w, dtype=torch.float)# new tensir(CHW)
+#     for k in mapping:
+#         v=mapping.get(k)
+#         # w:가로 h: 세로
+#         for w, h in itertools.product(range(w),range(h)):
+#             label[v][h][w]=(pix[w, h]==k)
+#     return label ## tensor
+
+# 두장인풋으로너어서
+def mask2label(mask_ori):## RGB tensor
+    
+    mask_bg = mask_ori.mul(-1.0).add(1.0)
+    label = torch.concat([mask_bg, mask_ori], dim=0)
+            
     return label ## tensor
